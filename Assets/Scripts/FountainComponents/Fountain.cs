511d1ab6f6df98ae3e3b98bc8a188cb3def11d03
Connor <connorbp@hotmail.com>
2019-03-04 12:26:23 -0700

    Made Fountain Dynamic, still broken

--- Assets/Scripts/FountainComponents/Fountain.cs
+++ Assets/Scripts/FountainComponents/Fountain.cs
@@ -9,28 +9,19 @@ public class Fountain : MonoBehaviour {
     //public float Height;
     //public float speed;
 
-    public float VelocityPercentage;
-    public float Diameter;
-    public float Velocity;
-    public float MaxFlowRate;
-    public float MaxPressure;
-    public float WaterDensity;
-
-
-    public Color Color;
-    public Light SpotLight;
-
+    public GameObject Pump;
+    public GameObject Nozzel;
+    public GameObject Light;
     public ParticleSystem Water;
-
     public float delay;
 
-    public UnityEvent Pattern;
-    
+    private float _waterDensity = 1000f;
+    private float _velocity;    
 
     // Use this for initialization
     void Start () {
         var shape = Water.shape;
-        shape.radius = Diameter;
+        shape.radius = Nozzel.GetComponent<Nozzel>().Diameter;
     }
 	
 	// Update is called once per frame
@@ -39,19 +30,19 @@ public class Fountain : MonoBehaviour {
         //Water.startSpeed = PatternFunctions.SinWave(Height, speed, delay);
         //Water.startSpeed = PatternFunctions.VShape(Height, speed, delay);
 
-        Velocity = Mathf.Sqrt((Mathf.Pow(MaxFlowRate, 2) * MaxPressure * 135.1473154f) / ((2122089428f * Mathf.Pow(Diameter, 4) * MaxPressure + (Mathf.Pow(MaxFlowRate, 2) ))* Mathf.Pow(Mathf.PI, 2)));
-        Water.startSpeed = Velocity * VelocityPercentage;
+        _velocity = Mathf.Sqrt((Mathf.Pow(Pump.GetComponent<Pump>().MaxFlowRate, 2) * Pump.GetComponent<Pump>().MaxPressure * 135.1473154f) / ((2122089428f * Mathf.Pow(Nozzel.GetComponent<Nozzel>().Diameter, 4) * Pump.GetComponent<Pump>().MaxPressure + (Mathf.Pow(Pump.GetComponent<Pump>().MaxFlowRate, 2) ))* Mathf.Pow(Mathf.PI, 2)));
+        Water.startSpeed = _velocity * Pump.GetComponent<Pump>().VelocityPercentage;
 
 
     }
 
     public void AdjustHeight(float percent)
     {
-        VelocityPercentage = percent;
+        Pump.GetComponent<Pump>().VelocityPercentage = percent;
     }
 
     public void ChangeColor(float red, float green, float blue)
     {
-        SpotLight.color = new Color(red, green, blue);
+        Light.GetComponent<Lighting>().Light.color = new Color(red, green, blue);
     }
 }
---
+++ Assets/Scripts/FountainComponents/Pump.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pump : MonoBehaviour {
+
+    public float VelocityPercentage;
+    public float MaxFlowRate;
+    public float MaxPressure;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
